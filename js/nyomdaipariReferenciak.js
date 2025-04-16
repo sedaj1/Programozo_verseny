a35bbc279d3909261efe02959dded7ee46b90f48
sedaj1 <scheda.balint.jozsef.2022i@bankitatabanya.hu>
2025-04-16 10:53:28 +0200

    elkezdtuk a jst

---
+++ js/nyomdaipariReferenciak.js
@@ -0,0 +1,101 @@
+const nyomdaipariReferenciak = [
+  {
+    nev: "Névjegykártya",
+    megrendelo: "XYZ Kft.",
+    meret: "90x50 mm",
+    szinek: "1+1",
+    darabszam: 500,
+    papirminoseg: "300g matt műnyomó",
+    kivitelezes: "sarkított, fóliázott",
+    tema: "divat",
+    kep: "https://cdn1.getprintbox.com/pbx2-printpix/media/uploads/new-nevjegykartya-keszites-klasszikus_jctymn.jpg"
+  },
+  {
+    nev: "Névjegykártya",
+    megrendelo: "ABC Bt.",
+    meret: "85x55 mm",
+    szinek: "4+0",
+    darabszam: 1000,
+    papirminoseg: "350g fényes műnyomó",
+    kivitelezes: "lakkozott",
+    tema: "üzleti",
+    kep: "https://cdn1.getprintbox.com/pbx2-printpix/media/uploads/new-nevjegykartya-keszites-exkluziv_qtmvpf.jpg"
+  },
+  {
+    nev: "Szórólap",
+    megrendelo: "GreenTech Zrt.",
+    meret: "A4",
+    szinek: "4+4",
+    darabszam: 1000,
+    papirminoseg: "150g fényes műnyomó",
+    kivitelezes: "tűzött",
+    tema: "környezetvédelem",
+    kep: "https://premiernyomda.hu/wp-content/uploads/2014/03/szorolap-premier-nyomda.jpg"
+  },
+  {
+    nev: "Katalógus",
+    megrendelo: "Autócentrum Bt.",
+    meret: "A5",
+    szinek: "4+4",
+    darabszam: 750,
+    papirminoseg: "170g matt műnyomó",
+    kivitelezes: "spirálozott",
+    tema: "autóipar",
+    kep: "https://www.printpix.hu/images/2020-termekoldalak/Termekek-szolgaltatasok/Katalogus-brossura/katalogus-nyomtatas-fokep.jpg"
+  },
+  {
+    nev: "Meghívó",
+    megrendelo: "DreamEvents Kft.",
+    meret: "120x180 mm",
+    szinek: "2+0",
+    darabszam: 200,
+    papirminoseg: "250g kreatív papír",
+    kivitelezes: "arany fólia, dombor nyomás",
+    tema: "esemény / esküvő",
+    kep: "https://amistyle-grafika.hu/wp-content/uploads/2023/10/Winter-Dream-Egyoldalas-Eskuvoi-meghivo-Teli-eskuvore-125x1755__2.jpg"
+  },
+  {
+    nev: "Csomagolás design",
+    megrendelo: "BioFood Kft.",
+    meret: "80x80x40 mm (doboz)",
+    szinek: "4+0",
+    darabszam: 5000,
+    papirminoseg: "mikrohullámú karton",
+    kivitelezes: "stancolt, ragasztott",
+    tema: "élelmiszeripar",
+    kep: "https://s3u.tmimgcdn.com/800x0/u78165984/06c24c25021765b856dd3ab2c2b437ae.jpg"
+  },
+  {
+    nev: "Szórólap",
+    megrendelo: "FitLife Kft.",
+    meret: "A5",
+    szinek: "4+0",
+    darabszam: 2000,
+    papirminoseg: "135g fényes műnyomó",
+    kivitelezes: "vágott",
+    tema: "egészség és fitness",
+    kep: "https://premiernyomda.hu/wp-content/uploads/2014/03/leaflet2.jpg"
+  },
+  {
+    nev: "Plakát",
+    megrendelo: "Művészeti Galéria",
+    meret: "A2",
+    szinek: "4+0",
+    darabszam: 300,
+    papirminoseg: "200g fényes műnyomó",
+    kivitelezes: "vágott",
+    tema: "művészet",
+    kep: "https://upload.wikimedia.org/wikipedia/commons/e/e3/CALANCHI6aediz.jpg"
+  },
+  {
+    nev: "Menükártya",
+    megrendelo: "Gourmet Étterem",
+    meret: "210x297 mm",
+    szinek: "4+4",
+    darabszam: 150,
+    papirminoseg: "250g matt műnyomó",
+    kivitelezes: "laminált",
+    tema: "gasztronómia",
+    kep: "https://www.meghivolap.hu/cdn/shop/products/leveles_menukartya_mockup_alkep_900x.jpg"
+  }
+  ];
